1d464e6f3d89e8e7abb31e2f56ecd85ed970df10
Akshay Ravi <58636452+AkshayraviC09YC47@users.noreply.github.com>
2019-12-21 12:12:10 +0530

    Add files via upload

---
+++ macspoofer.py
@@ -0,0 +1,48 @@
+#usr/bin/env python3
+
+#This program has been created by Akshay Ravi
+#This is a random mac changer
+
+import os
+import subprocess
+
+os.system("clear")
+
+banner='''
+                                __                    __           
+             /\/\   __ _  ___  / _\_ __   ___   ___  / _| ___ _ __ 
+            /    \ / _` |/ __| \ \| '_ \ / _ \ / _ \| |_ / _ \ '__|
+           / /\/\ \ (_| | (__  _\ \ |_) | (_) | (_) |  _|  __/ |   
+           \/    \/\__,_|\___| \__/ .__/ \___/ \___/|_|  \___|_|   
+                                  |_|                              
+           --------------------------------------------------------
+               Random Mac Address Spoofer Created By Akshay Ravi
+           --------------------------------------------------------
+'''
+
+print(banner)
+
+mac=input(str("[*] Do you want to see your current MAC Address?[y/n]: "))
+if mac =="y":
+  print("")
+  subprocess.call("ifconfig" , shell=True)
+elif mac =="n":
+  value=input(str("[*] Do you want to spoof yor MAC Address?[y/n]: "))
+if value =="y":
+  interface=input(str("[*] Enter your interface (eth0,wlan0,wlan1,etc): "))
+  print("[*] Spoofing MAC Address.....")
+  print("[*] Your new MAC Address:")
+  print("")
+  subprocess.call("macchanger -r "+interface,shell=True)
+  print("")
+  
+  value1=input(str("[*] Do you want to reset yor MAC Address?[y/n]: "))
+if value1 == "y":
+          print("[*] MAC Address has been reset sucessfully")
+          print("")
+          subprocess.call("macchanger -p "+interface , shell=True)
+elif value =="n":
+  print("[*] Thank you for using MAC Spoofer")
+  exit(1)
+  
+
